2555ab938607047d439a42f87fe41964a09fb2c1
Juan Felipe Botero <juanfbl9307@gmail.com>
2022-02-09 05:28:56 -0500

    dockerfile added

--- app_one/app.js
+++ app_one/app.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const axios = require("axios");
 const app = express()
-const port = 3000
+const port = process.env.APP_PORT
 
-app.get('/', (req, res) => {
-    res.send('Hello From App One!')
+async function getByAxios() {
+    try {
+        const axiosResponse = await axios.get(
+            'http://twoapp/say-hello',
+        );
+        return axiosResponse.data;
+    } catch (e) {
+        console.log(e);
+        return "... no one response";
+    }
+}
+
+app.get('/app-two', async (req, res) => {
+    const response = await getByAxios();
+    res.send(`Who is there? ${response}`);
+})
+
+app.get('/', async (req, res) => {
+    res.send('Hello From App One!');
+})
+
+app.get('/say-hello', (req, res) => {
+    res.send('Hey, here App One!');
 })
 
 app.listen(port, () => {
-    console.log(`App One running on port ${port}`)
+    console.log(`App One running on port ${port}`);
 })
--- app_two/app.js
+++ app_two/app.js
@@ -1,11 +1,34 @@
-const express = require('express')
+const axios = require("axios");
+const express = require("express");
 const app = express()
-const port = 3001
+const port = process.env.APP_PORT
+
+async function getByAxios() {
+    try {
+        const axiosResponse = await axios.get(
+            'http://oneapp/say-hello',
+        );
+        return axiosResponse.data;
+    } catch (e) {
+        console.log(e);
+        return "... no one response";
+    }
+}
+
+app.get('/app-one', async (req, res) => {
+    const response = await getByAxios();
+    res.send(`Who is there? ${response}`);
+});
+
 
 app.get('/', (req, res) => {
-    res.send('Hello From App Two!')
-})
+    res.send('Hello From App Two!');
+});
+
+app.get('/say-hello', (req, res) => {
+    res.send('Hey, here App Two!');
+});
 
 app.listen(port, () => {
-    console.log(`App One running on port ${port}`)
-})
+    console.log(`App Two running on port ${port}`);
+});
